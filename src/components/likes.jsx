ddb047984f7103449af3b7a52e8462b39facdcf0
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-04-04 19:19:14 -0400

    improve stying of likes component

--- src/components/likes.jsx
+++ src/components/likes.jsx
@@ -2,8 +2,8 @@
 export default function Likes({storage, remove}){
     let key = 0
     const likeList = storage.map(store =>
-        <div key={key} className=" grid grid-cols-6">
-            <li className=" marker:content-['-'] indent-3 py-1 mx-1 col-span-5" key={key++}>{store}</li>
+        <div key={key} className=" grid grid-cols-6 my-3 min-h-[3.4rem] items-center">
+            <li className=" col-span-5" key={key++}>{store}</li>
             <button key={key} onClick = {remove} className=" py-2 px-4 w-fit h-fit bg-gray-700 text-green-400 hover:bg-gray-600 justify-self-center rounded-full border border-transparent">
                 ×
             </button>
@@ -12,7 +12,7 @@ export default function Likes({storage, remove}){
     return(
         <section className="bg-gray-800 rounded-2xl max-w-md md:min-w-full mx-auto my-12 p-8">
             <h2 className=" text-green-400 py-2 font-bold text-center text-xl">Likes</h2>
-            <ul className="text-slate-200 text-xl p-6 max-h-[28rem] overflow-y-scroll">{likeList}</ul>
+            <ul className="text-slate-200 text-xl p-4 max-h-[28rem] overflow-y-scroll">{likeList}</ul>
         </section>
     )
 }
